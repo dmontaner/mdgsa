4bae812d810e934d30cfa63b64b11261118f06fa
dmontaner <david.montaner@gmail.com>
2014-10-10 00:44:20 +0200

    goLeaves accepts a vector now

--- R/goLeaves.r
+++ R/goLeaves.r
@@ -23,6 +23,11 @@
 ##' @details
 ##' Uses the library GO.db to find the 'ancestors' of each GO term.
 ##' Those ancestors are discarded form the \code{uvGsa} results.
+##'
+##' Alternativeliy, the function may also take a character vector of GO ids
+##' in the \code{gsaout} parameter.
+##' In such case the function returns also a character vector of GO ids,
+##' containing just the GO terms being "leaves" of the original set.
 ##' 
 ##' @param gsaout data.frame; output from uvGsa.
 ##' @param cutoff p-value cutoff for considering significant a Gene Set.
@@ -39,44 +44,57 @@
 ##' @export
 
 goLeaves <- function (gsaout, cutoff = 0.05, pvalue = "padj", statistic = "lor", verbose = TRUE, sort = TRUE) {
+    
+    if (verbose) {
+        message ("\n", "Using GO.db version: ", packageDescription ("GO.db", fields = "Version")) #2.3.5
+    }
+    
+    ancestros <- c (as.list (GOBPANCESTOR), as.list (GOMFANCESTOR), as.list (GOCCANCESTOR))  
+  
+    ## ###########################################################################
 
-  if (verbose) {
-    message ("\n", "Using GO.db version: ", packageDescription ("GO.db", fields = "Version")) #2.3.5
-  }
+    ## real gsaout
+    if (is.data.frame (gsaout) | is.matrix (gsaout)) {
   
-  ancestros <- c (as.list (GOBPANCESTOR), as.list (GOMFANCESTOR), as.list (GOCCANCESTOR))  
+        pat <- uvPat (gsaout, cutoff = cutoff, pvalue = pvalue, statistic = statistic)
   
-  ## ###########################################################################
+        touse <- pat %in% c (-1, 1)
+        gsaout <- gsaout[touse, , drop = FALSE]
+        pat <- pat[touse]
   
-  pat <- uvPat (gsaout, cutoff = cutoff, pvalue = pvalue, statistic = statistic)
+        is.up <- pat == 1
+        is.do <- pat ==-1
   
-  touse <- pat %in% c(-1, 1)
-  gsaout <- gsaout[touse, , drop = FALSE]
-  pat <- pat[touse]
+        gos.up <- rownames (gsaout)[is.up]
+        gos.do <- rownames (gsaout)[is.do]
   
-  is.up <- pat == 1
-  is.do <- pat ==-1
-  
-  gos.up <- rownames (gsaout)[is.up]
-  gos.do <- rownames (gsaout)[is.do]
-  
-  anc.up <- unique (unlist (ancestros[gos.up]))
-  anc.do <- unique (unlist (ancestros[gos.do]))
+        anc.up <- unique (unlist (ancestros[gos.up]))
+        anc.do <- unique (unlist (ancestros[gos.do]))
 
-  leaves.up <- setdiff (gos.up, anc.up)
-  leaves.do <- setdiff (gos.do, anc.do)
+        leaves.up <- setdiff (gos.up, anc.up)
+        leaves.do <- setdiff (gos.do, anc.do)
 
-  leaves <- c(leaves.up, leaves.do)
-  touse <- rownames (gsaout) %in% leaves
-  gsaout <- gsaout[touse, , drop = FALSE]
+        leaves <- c(leaves.up, leaves.do)
+        touse <- rownames (gsaout) %in% leaves
+        gsaout <- gsaout[touse, , drop = FALSE]
 
-  ##sort
-  if (sort) {
-    myindex <- pval2index (pval = gsaout[,pvalue], sign = gsaout[,statistic], log = TRUE, verbose = verbose)
-    orden <- order (myindex, decreasing = TRUE)
-    gsaout <- gsaout[orden,]
-  }
+        ##sort
+        if (sort) {
+            myindex <- pval2index (pval = gsaout[,pvalue], sign = gsaout[,statistic], log = TRUE, verbose = verbose)
+            orden <- order (myindex, decreasing = TRUE)
+            gsaout <- gsaout[orden,]
+        }
+    }
+
+    ## ###########################################################################
+
+    ## character verctor of GO ids
+    if (is.character (gsaout)) {
+        anc <- unique (unlist (ancestros[gsaout]))
+        leaves <- setdiff (gsaout, anc)
+        gsaout <- leaves
+    }
   
-  ##RETURN
-  return (gsaout)
+    ## RETURN
+    return (gsaout)
 }
